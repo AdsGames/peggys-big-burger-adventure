c3e633969b5d0f048830ae0f2195624b4510af1f
Sullivan Stobo <stobos@mcmaster.ca>
2020-05-08 19:47:24 -0400

    Movement code in the works

---
+++ PeggySim/Assets/Movement.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //publics to be provided in editor
+    public float turnSpeed;
+    public float moveSpeed;
+    public bool movementReversed;
+    public bool turningReversed;
+    public float drag;
+    public float angularDrag;
+
+    public List<GameObject> wheels;
+
+    private GameObject head;
+
+    private float speed = 10;
+    private float rotation_speed = 2;
+
+    public Rigidbody m_Rigidbody;
+    Vector3 m_EulerAngleVelocity;
+
+    private bool onGround = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   //Set the axis the Rigidbody rotates in (100 in the y axis)
+        m_EulerAngleVelocity = new Vector3(0, 100, 0);
+
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        m_Rigidbody.drag = drag;
+        m_Rigidbody.angularDrag = angularDrag;
+
+        wheels = new List<GameObject>();
+        foreach(Transform child in transform)
+        {
+            Debug.Log(child.tag);
+            if (child.tag == "Leg")
+            {
+                wheels.Add(child.gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        /* The Danno original
+        if (!Input.GetKey(KeyCode.W))
+            m_Rigidbody.MovePosition(m_Rigidbody.position + transform.forward * -speed * Time.fixedDeltaTime);
+
+
+        if (Input.GetKey(KeyCode.A)){
+                    Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * -Time.deltaTime);
+                     m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
+        }
+        if (Input.GetKey(KeyCode.Space)){
+           m_Rigidbody.AddRelativeForce(new Vector3(0,100,0));
+        }
+      
+        if (Input.GetKey(KeyCode.D)){
+            
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
+        }
+        */
+        foreach(GameObject child in wheels)
+        {
+            if (child.GetComponent<Wheel>().onGround)
+            {
+                onGround = true;
+                break;
+            }
+        }
+
+        if (onGround)
+        {
+            m_Rigidbody.AddRelativeForce(new Vector3(0, 0, moveSpeed * (movementReversed ? 1 : -1)), ForceMode.Acceleration);
+
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                m_Rigidbody.AddRelativeTorque(new Vector3(0, turnSpeed * (turningReversed ? 1 : -1), 0), ForceMode.Acceleration);
+            }
+            else if (Input.GetAxis("Horizontal") > 0)
+            {
+                m_Rigidbody.AddRelativeTorque(new Vector3(0, -turnSpeed * (turningReversed ? 1 : -1), 0), ForceMode.Acceleration);
+            }
+        }
+    }
+}
---
+++ PeggySim/Assets/Wheel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+
+    public WheelCollider collider;
+    public bool onGround;
+
+    void Start()
+    {
+        collider = GetComponent<WheelCollider>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Segment" || collision.gameObject.tag == "Segment")
+        {
+            Physics.IgnoreCollision(collision.collider, collider);
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("Bork Brok no longer on the ground");
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground" && !onGround)
+        {
+            Debug.Log("Bork bork on the ground");
+            onGround = true;
+        }
+    }
+}
