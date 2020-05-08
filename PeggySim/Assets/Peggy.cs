a4023c079f623b7466aa6b74a6fec3ccb4482405
Danny Van Stemp <danwardvs@adsgames.net>
2020-05-08 14:35:26 -0400

    Basic peggy PC

---
+++ PeggySim/Assets/Peggy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peggy : MonoBehaviour
+{
+
+    private GameObject head;
+    private float speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //head =  this.transform.Find("Head");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)){
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A)){
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        
+        
+
+    }
+}
