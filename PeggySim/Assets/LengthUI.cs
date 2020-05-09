d072932c6e9f9f4d29aa92698a4a9ae1b4775913
SullivanStobo <ss.stobo@gmail.com>
2020-05-09 14:29:33 -0400

    Good morning sully (#37)
    * Added length UI
    
    * Super best camera meleee
    
    Co-authored-by: Sullivan Stobo <stobos@mcmaster.ca>

---
+++ PeggySim/Assets/LengthUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LengthUI : MonoBehaviour
+{
+    private Text LengthText;
+
+    void Start()
+    {
+        LengthText = GetComponent<Text>();
+    }
+
+    void FixedUpdate()
+    {
+        LengthText.text = GameObject.FindGameObjectWithTag("GameInfo").GetComponent<GameInfo>().getSegments().ToString();
+    }
+}
