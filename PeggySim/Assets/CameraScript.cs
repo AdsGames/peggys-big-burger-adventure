7665d6ee59d691562d9da742cd495b4eb327dd0e
Danny Van Stemp <danwardvs@adsgames.net>
2020-05-09 04:52:45 -0400

    Material update (#32)
    * Got some normal maps up in this b
    
    * Rest of material committed and dynamic camera

---
+++ PeggySim/Assets/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private GameInfo info;
+    public float segmentScale = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        info = GameObject.FindGameObjectWithTag("GameInfo").GetComponent<GameInfo>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        transform.localPosition = new Vector3(0,3.5f + (info.getSegments()*segmentScale),5.5f +(info.getSegments()*segmentScale));
+    }
+}
