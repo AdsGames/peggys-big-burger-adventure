c2322f91b324324bda8450295446cdda13f8f144
Danny Van Stemp <danwardvs@adsgames.net>
2020-05-08 22:05:51 -0400

    Head and butt and better connections between links

--- PeggySim/Assets/Peggy.cs
+++ PeggySim/Assets/Peggy.cs
@@ -16,33 +16,21 @@ public class Peggy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-   //Set the axis the Rigidbody rotates in (100 in the y axis)
-        m_EulerAngleVelocity = new Vector3(0, 100, 0);
 
         //Fetch the Rigidbody from the GameObject with this script attached
         m_Rigidbody = GetComponent<Rigidbody>();
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        print(currentBodyStart.transform.position);
-        if (!Input.GetKey(KeyCode.W))
-            m_Rigidbody.MovePosition(m_Rigidbody.position + transform.forward * -speed * Time.deltaTime);
 
-
-        if (Input.GetKey(KeyCode.A)){
-                    Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * -Time.deltaTime);
-                     m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
-        }
-        if (Input.GetKeyDown(KeyCode.Space)){
-           m_Rigidbody.AddRelativeForce(new Vector3(0,1000,0));
-        }
-        */
         if (Input.GetKeyDown(KeyCode.F)){
-            Vector3 pos = currentBodyStart.transform.position + transform.forward * 1.0f;
+           
+            Vector3 pos = currentBodyStart.transform.position +  transform.forward * 1.0f;
             currentBodyStart.transform.position = pos;
             GameObject new_segment = Instantiate(bodySegmentPrefab, transform);
             new_segment.GetComponent<CharacterJoint>().connectedBody = m_Rigidbody;
@@ -53,21 +41,6 @@ public class Peggy : MonoBehaviour
 
            currentBodyStart = new_segment;
         }
-        /*
-        if (Input.GetKey(KeyCode.G)){
-            
-            currentBodyStart.transform.rotation = Quaternion.Euler(0, 0, 0);
-            
-        }
-      
-        if (Input.GetKey(KeyCode.D)){
-            
-        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
-        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
-        }
-        */
-        
-        
 
     }
 }
