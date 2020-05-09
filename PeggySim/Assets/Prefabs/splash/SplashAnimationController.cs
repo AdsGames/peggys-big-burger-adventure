f900200a46dbce81156bd606d609c0e601bab043
Allan Legemaate <alegemaate@gmail.com>
2020-05-09 15:18:23 -0400

    Add splash screen

---
+++ PeggySim/Assets/Prefabs/splash/SplashAnimationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SplashAnimationController : MonoBehaviour
+{
+  private Animation animation;
+
+  private void Start()
+  {
+    animation = gameObject.GetComponent<Animation>();
+    animation.Play("FadeIn");
+
+  }
+
+  private void Update()
+  {
+  }
+}
