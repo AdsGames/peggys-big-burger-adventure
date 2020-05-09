3d664965351a035ef6d5c7df2aee7e4a73786d5e
Danny Van Stemp <danwardvs@adsgames.net>
2020-05-09 16:52:29 -0400

    Respanwesr are here and working

---
+++ PeggySim/Assets/RespawnScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float respawnTime=5;
+    public float spawnHeight=1;
+    private float time=99999;
+    public GameObject item;
+    private GameObject spawnedItem;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnedItem==null){
+            time+=Time.deltaTime;
+        }
+        
+        if(time>=respawnTime){
+            time=0;
+            spawnedItem = Instantiate(item);
+ 
+
+            spawnedItem.transform.position = new Vector3(transform.position.x,spawnHeight,transform.position.z);
+
+            print("borger");
+        }
+    }
+}
