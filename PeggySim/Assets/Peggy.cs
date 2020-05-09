c8d09f806f79f542ff20f641f1af8614b93315ea
SullivanStobo <ss.stobo@gmail.com>
2020-05-08 20:41:30 -0400

    Movement + Danno
    * Peggy can now be extended and it works kinda decently
    
    * Movement code in the works
    
    * Wheely peggly
    
    * Movement
    
    * Made adding segments work with spicy movement
    
    Co-authored-by: Danny Van Stemp <danwardvs@adsgames.net>
    Co-authored-by: Sullivan Stobo <stobos@mcmaster.ca>

--- PeggySim/Assets/Peggy.cs
+++ PeggySim/Assets/Peggy.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Peggy : MonoBehaviour
 {
 
-    private GameObject head;
+    public GameObject bodySegmentPrefab;
+    public GameObject currentBodyStart;
 
     private float speed = 10;
     private float rotation_speed = 2;
@@ -20,21 +21,43 @@ public class Peggy : MonoBehaviour
 
         //Fetch the Rigidbody from the GameObject with this script attached
         m_Rigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
+        print(currentBodyStart.transform.position);
         if (!Input.GetKey(KeyCode.W))
-            m_Rigidbody.MovePosition(m_Rigidbody.position + transform.forward * -speed * Time.fixedDeltaTime);
+            m_Rigidbody.MovePosition(m_Rigidbody.position + transform.forward * -speed * Time.deltaTime);
 
 
         if (Input.GetKey(KeyCode.A)){
                     Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * -Time.deltaTime);
                      m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
         }
-        if (Input.GetKey(KeyCode.Space)){
-           m_Rigidbody.AddRelativeForce(new Vector3(0,100,0));
+        if (Input.GetKeyDown(KeyCode.Space)){
+           m_Rigidbody.AddRelativeForce(new Vector3(0,1000,0));
+        }
+        */
+        if (Input.GetKeyDown(KeyCode.F)){
+            Vector3 pos = currentBodyStart.transform.position + transform.forward * 1.0f;
+            currentBodyStart.transform.position = pos;
+            GameObject new_segment = Instantiate(bodySegmentPrefab, transform);
+            new_segment.GetComponent<CharacterJoint>().connectedBody = m_Rigidbody;
+
+           currentBodyStart.GetComponent<CharacterJoint>().connectedBody = new_segment.GetComponent<Rigidbody>();
+           
+           currentBodyStart.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+
+           currentBodyStart = new_segment;
+        }
+        /*
+        if (Input.GetKey(KeyCode.G)){
+            
+            currentBodyStart.transform.rotation = Quaternion.Euler(0, 0, 0);
+            
         }
       
         if (Input.GetKey(KeyCode.D)){
@@ -42,6 +65,7 @@ public class Peggy : MonoBehaviour
         Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
         m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
         }
+        */
         
         
 
---
+++ PeggySim/Assets/Wheel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+
+    public WheelCollider collider;
+    public bool onGround;
+
+    void Start()
+    {
+        collider = GetComponent<WheelCollider>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Segment" || collision.gameObject.tag == "Head")
+        {
+            Physics.IgnoreCollision(collision.collider, collider);
+        }
+
+    }
+}
