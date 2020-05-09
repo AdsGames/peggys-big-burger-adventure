5ff5ee6a873924a8804795284280f7d2bcd89b4e
SullivanStobo <ss.stobo@gmail.com>
2020-05-09 00:29:52 -0400

    Sully best branch (#28)
    * Peggy can now be extended and it works kinda decently
    
    * Movement code in the works
    
    * Wheely peggly
    
    * Movement
    
    * Made adding segments work with spicy movement
    
    * Get more faster with more segments!
    
    * Trying again
    
    * Fixed pego prefab
    
    * Amazing materials. Also danny is lame
    
    * Mega peggo
    
    * Segment stability
    
    Co-authored-by: Danny Van Stemp <danwardvs@adsgames.net>
    Co-authored-by: Sullivan Stobo <stobos@mcmaster.ca>

--- PeggySim/Assets/Movement.cs
+++ PeggySim/Assets/Movement.cs
@@ -74,6 +74,10 @@ public class Movement : MonoBehaviour
 
         if (onGround)
         {
+            m_Rigidbody.drag = drag;
+            m_Rigidbody.angularDrag = angularDrag;
+
+
             m_Rigidbody.AddRelativeForce(new Vector3(0, 0, moveSpeed * (movementReversed ? 1 : -1) * ((info.getSegments()* segmentFactor)+ 1)), ForceMode.Acceleration);
 
             if (Input.GetAxis("Horizontal") < 0)
@@ -90,7 +94,12 @@ public class Movement : MonoBehaviour
                 m_Rigidbody.AddRelativeForce(new Vector3(0, 50, 0), ForceMode.Impulse);
             }
         }
-        Debug.Log(info.getSegments());
+        else
+        {
+            m_Rigidbody.drag = 0;
+            m_Rigidbody.angularDrag = 0;
+
+        }
     }
     private void OnTriggerStay(Collider other)
     {
