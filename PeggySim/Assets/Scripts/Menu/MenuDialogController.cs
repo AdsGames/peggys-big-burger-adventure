539e07294d781a32f39abd2dbdbb6b39856ad7d6
Allan Legemaate <alegemaate@gmail.com>
2020-05-08 20:42:52 -0400

    Add menu scene, fonts and scripts folder (#16)

---
+++ PeggySim/Assets/Scripts/Menu/MenuDialogController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class MenuDialogController : MonoBehaviour
+{
+  public Button closeButton;
+  public Button openButton;
+  public GameObject canvas;
+
+  public void Start()
+  {
+    if (closeButton != null) {
+      Button btn = closeButton.GetComponent<Button>();
+      btn.onClick.AddListener(HandleClose);
+    }
+
+    if (openButton != null) {
+      Button btn = openButton.GetComponent<Button>();
+      btn.onClick.AddListener(HandleOpen);
+    }
+  }
+
+  private void HandleClose()
+  {
+    if (canvas != null) {
+      canvas.SetActive(false);
+    }
+  }
+
+  private void HandleOpen()
+  {
+    if (canvas != null) {
+      canvas.SetActive(true);
+    }
+  }
+}
