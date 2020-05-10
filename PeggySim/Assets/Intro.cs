61aef58fa83d337d0b7b4190b4e01f3b4734bade
Sullivan Stobo <stobos@mcmaster.ca>
2020-05-09 20:32:41 -0400

    Beautiful countdown

--- PeggySim/Assets/Intro.cs
+++ PeggySim/Assets/Intro.cs
@@ -7,9 +7,17 @@ public class Intro : MonoBehaviour
 {
     private Animation anim;
     private Text introText;
+
+    public GameObject three;
+    public GameObject two;
+    public GameObject one;
+    public GameObject go;
+
+    public List<AudioClip> audio;
+
     void Start()
     {
-        anim = GetComponent<Animation>();
+
         introText = GetComponent<Text>();
         StartCoroutine(playIntro());
     }
@@ -22,16 +30,49 @@ public class Intro : MonoBehaviour
 
     private IEnumerator playIntro()
     {
-        anim.Play("3");
-        yield return new WaitForSeconds(1f);
-        introText.text = "2";
-        anim.Play("2");
-        yield return new WaitForSeconds(1f);
-        introText.text = "1";
-        anim.Play("1");
-        yield return new WaitForSeconds(1f);
-        introText.text = "Go!";
-        anim.Play("Go");
+        GameObject.FindGameObjectWithTag("Head").GetComponent<Movement>().enabled = false;
+        GameObject.Find("MainMusic").GetComponent<AudioSource>().enabled = false;
+
+        yield return new WaitForSeconds(0.3f);
+
+        three.transform.GetChild(0).GetComponent<Text>().enabled = true;
+        three.GetComponent<Animator>().Play("3");
+
+        yield return new WaitForSeconds(0.5f);
+        GetComponent<AudioSource>().clip = audio[0];
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(0.5f);
+        three.transform.GetChild(0).GetComponent<Text>().enabled = false;
+
+        two.transform.GetChild(0).GetComponent<Text>().enabled = true;
+        two.GetComponent<Animator>().Play("2");
+
+        yield return new WaitForSeconds(0.5f);
+        GetComponent<AudioSource>().clip = audio[1];
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(0.5f);
+        two.transform.GetChild(0).GetComponent<Text>().enabled = false;
+       
+        one.transform.GetChild(0).GetComponent<Text>().enabled = true;
+        one.GetComponent<Animator>().Play("1");
+
+        yield return new WaitForSeconds(0.5f);
+        GetComponent<AudioSource>().clip = audio[2];
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(0.5f);
+        one.transform.GetChild(0).GetComponent<Text>().enabled = false;
+
+        go.transform.GetChild(0).GetComponent<Text>().enabled = true;
+        go.GetComponent<Animator>().Play("Go");
+
+        yield return new WaitForSeconds(0.3f);
+        GetComponent<AudioSource>().clip = audio[3];
+        GetComponent<AudioSource>().Play();
+
+        GameObject.FindGameObjectWithTag("Head").GetComponent<Movement>().enabled = true;
+        yield return new WaitForSeconds(0.7f);
+        GameObject.Find("MainMusic").GetComponent<AudioSource>().enabled = true;
+        go.transform.GetChild(0).GetComponent<Text>().enabled = false;
         Debug.Log("Ding!");
     }
 }
--- PeggySim/Assets/SpeedUI.cs
+++ PeggySim/Assets/SpeedUI.cs
@@ -19,9 +19,12 @@ public class SpeedUI : MonoBehaviour
 
     void FixedUpdate()
     {
-        speedText.text = GameObject.FindGameObjectWithTag("Head").GetComponent<Rigidbody>().velocity.magnitude.ToString().Substring(0, 4);
-        speedColour.r = GameObject.FindGameObjectWithTag("Head").GetComponent<Rigidbody>().velocity.magnitude / 50f;
-        speedColour.g = 1 - speedColour.r;
-        speedText.color = speedColour;
+        if (GameObject.FindGameObjectWithTag("Head").GetComponent<Rigidbody>().velocity.magnitude != 0.0f)
+        {
+            speedText.text = GameObject.FindGameObjectWithTag("Head").GetComponent<Rigidbody>().velocity.magnitude.ToString().Substring(0, 4);
+            speedColour.r = GameObject.FindGameObjectWithTag("Head").GetComponent<Rigidbody>().velocity.magnitude / 50f;
+            speedColour.g = 1 - speedColour.r;
+            speedText.color = speedColour;
+        }
     }
 }
