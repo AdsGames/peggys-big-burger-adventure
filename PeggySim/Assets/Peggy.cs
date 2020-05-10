c177e4d9a61c3f8844dce4911f9eeebdca459555
Danny Van Stemp <danwardvs@adsgames.net>
2020-05-10 13:44:57 -0400

    butt reattachement is 100x better

--- PeggySim/Assets/Peggy.cs
+++ PeggySim/Assets/Peggy.cs
@@ -4,6 +4,7 @@ public class Peggy : MonoBehaviour
 {
   public GameObject bodySegmentPrefab = null;
   public GameObject currentBodyStart = null;
+  public GameObject itemParticle;
   public float breakTimer=0;
   private Rigidbody rigidBody = null;
 
@@ -26,20 +27,20 @@ public class Peggy : MonoBehaviour
   }
    public void handleButtCollision(Collider other){
         if(breakTimer>2){
-        GameObject[] list = GameObject.FindGameObjectsWithTag("Segment");
-        GameObject oldest = list[0];
-        for(int i=0; i<list.Length; i++){
-            if(oldest.GetComponent<Segment>().getAge()>list[i].GetComponent<Segment>().getAge())
-                oldest = list[i];
-        }
-        other.gameObject.transform.position = oldest.transform.position;
-        other.gameObject.transform.rotation = oldest.transform.rotation;
+            GameObject[] list = GameObject.FindGameObjectsWithTag("Segment");
+            GameObject oldest = list[0];
+            for(int i=0; i<list.Length; i++){
+                if(oldest.GetComponent<Segment>().getAge()>list[i].GetComponent<Segment>().getAge())
+                    oldest = list[i];
+              }
+            other.gameObject.transform.position = oldest.transform.position;
+            other.gameObject.transform.rotation = oldest.transform.rotation;
 
 
-        other.gameObject.GetComponent<HingeJoint>().connectedBody = oldest.GetComponent<Rigidbody>();
+            other.gameObject.GetComponent<HingeJoint>().connectedBody = oldest.GetComponent<Rigidbody>();
 
-        //GameObject new_particle = Instantiate(explodeParticle);
-        //new_particle.transform.position = other.transform.position;
+            GameObject new_particle = Instantiate(itemParticle);
+            new_particle.transform.position = transform.position;
         }
     }
 
